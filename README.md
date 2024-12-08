b52ec4104249987d0e4f6da35843bc75c04255ab
yamaguchi.takumi <gu.jp06042gmail.com>
2024-12-08 16:51:30 +0900

    Save changes for wordpress/wp-content/themes/warudakumi

---
+++ README.md
@@ -0,0 +1,10 @@
+<<<<<<< HEAD
+# warudakumi
+=======
+# warudakumi
+>>>>>>> cb75d0c (first commit)
+# warudakumi
+# warudakumi
+# warudakumi
+# warudakumi
+# warudakumi
---
+++ wordpress/wp-content/uploads/aios/firewall-rules/settings.php
@@ -0,0 +1,6 @@
+<?php __halt_compiler();
+/**
+ * This file was created by All In One Security (AIOS) plugin.
+ * The file is required for storing and retrieving your firewall's settings.
+ */
+{"aios_enable_rename_login_page":"","aios_login_page_slug":"","aios_enable_brute_force_attack_prevention":"","aios_brute_force_secret_word":"","aios_cookie_based_brute_force_redirect_url":"http:\/\/127.0.0.1","aios_brute_force_attack_prevention_pw_protected_exception":"","aios_brute_force_attack_prevention_ajax_exception":"","aios_brute_force_secret_cookie_name":"aios_brute_force_secret_152e1fceeaeab46f9d1b686d164bba4c","aios_brute_force_cookie_salt":"mE*YD-]P\/|7D7&e.x@wsO{]i#v]ROnR cUZxzpoh8gF@V7Hvszs=DAl$!ZM&|[jo49XKwSh8;I(U~9xZD_kw)+hA+0yZRDn?$sKLJqF7[!4Ne=I$KBi Wp,r,ZAQCc(A","aios_ip_retrieve_method":0,"aiowps_ip_retrieve_method":"","aiowps_blacklist_ips":[],"aiowps_blacklist_user_agents":[]}
